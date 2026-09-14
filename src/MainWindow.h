32070861d66f07bf4aa72cad0f42737656d48154
Mio Ngaolakorn <ngaolakhorny@gmail.com>
2026-09-14 18:05:12 +0700

    Simplify LiteWave without search API

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -9,7 +9,6 @@ class QProgressBar;
 class QTabWidget;
 class QWebEngineView;
 class QAction;
-class QNetworkAccessManager;
 
 class MainWindow final : public QMainWindow
 {
@@ -33,13 +32,11 @@ private:
     QProgressBar *progress_;
     QAction *shieldAction_;
     AdBlocker *adBlocker_;
-    QNetworkAccessManager *network_;
     bool darkMode_ = false;
 
     QWebEngineView *currentView() const;
     QWebEngineView *createView(const QUrl &url);
     void openUrl(const QString &text);
-    void searchGoogleApi(const QString &query);
     void applyTheme();
     void loadHome(QWebEngineView *view);
 };
