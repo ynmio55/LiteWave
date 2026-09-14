ee946c47be3f1f4ee4fac9d43a499d8520fabb14
Mio Ngaolakorn <ngaolakhorny@gmail.com>
2026-09-14 15:36:29 +0700

    Fix ad blocker toggle API

--- src/AdBlocker.h
+++ src/AdBlocker.h
@@ -5,5 +5,9 @@ class AdBlocker final : public QWebEngineUrlRequestInterceptor
 {
 public:
     explicit AdBlocker(QObject *parent = nullptr);
+    void setEnabled(bool enabled);
     void interceptRequest(QWebEngineUrlRequestInfo &info) override;
+
+private:
+    bool enabled_ = true;
 };
