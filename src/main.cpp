276bc6fc8939c3d27ac9251542b1266752b4a363
mioynx <mioynx@example.com>
2026-09-15 15:56:38 +0700

    feat: enable Secure DNS (DNS-over-HTTPS) via Chromium flags in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,45 @@
 
 #include <QApplication>
 #include <QIcon>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
-    // Do not override Chromium runtime flags: Qt WebEngine's defaults provide
-    // the widest compatibility with embedded players, login providers and
-    // regular websites.
+    QCoreApplication::setOrganizationName("LiteWave");
+    QCoreApplication::setApplicationName("LiteWave");
+    QCoreApplication::setApplicationVersion("1.0.1");
+
+    QSettings settings("LiteWave", "LiteWave");
+    bool secureDnsEnabled = settings.value("secureDnsEnabled", false).toBool();
+    QString provider = settings.value("dnsProvider", "OS Default").toString();
+    QString customUrl = settings.value("customDnsUrl", "").toString();
+
+    if (secureDnsEnabled && provider != "OS Default") {
+        QString templateUri;
+        if (provider == "Cloudflare") {
+            templateUri = "https://chrome.cloudflare-dns.com/dns-query";
+        } else if (provider == "Google") {
+            templateUri = "https://dns.google/dns-query{?dns}";
+        } else if (provider == "Quad9") {
+            templateUri = "https://dns.quad9.net/dns-query";
+        } else if (provider == "AdGuard") {
+            templateUri = "https://dns.adguard-dns.com/dns-query";
+        } else if (provider == "Custom" && !customUrl.isEmpty()) {
+            templateUri = customUrl;
+        }
+
+        if (!templateUri.isEmpty()) {
+            QString existingFlags = qgetenv("QTWEBENGINE_CHROMIUM_FLAGS");
+            QString dnsFlags = QString("--enable-features=DnsOverHttps --dns-over-https-templates=\"%1\" --dns-over-https-mode=secure").arg(templateUri);
+            if (!existingFlags.isEmpty()) {
+                dnsFlags = existingFlags + " " + dnsFlags;
+            }
+            qputenv("QTWEBENGINE_CHROMIUM_FLAGS", dnsFlags.toUtf8());
+        }
+    }
+
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     QApplication app(argc, argv);
-    QApplication::setOrganizationName("LiteWave");
-    QApplication::setApplicationName("LiteWave");
-    QApplication::setApplicationVersion("1.0.1");
     QGuiApplication::setDesktopFileName("LiteWave");
     app.setWindowIcon(QIcon(":/icons/litewave.svg"));
 
