56e66e432cbc6f7b3f6e241f320b5218c9a6a2ee
Mio Ngaolakorn <ngaolakhorny@gmail.com>
2026-09-15 18:06:21 +0700

    Fix Secure DNS settings compile error

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2904,7 +2904,8 @@ void MainWindow::showSettingsDialog() {
     pIdx = 0;
   // Avoid a confusing "enabled" state that still means OS DNS. The first
   // opt-in starts with Cloudflare; users can still choose any provider.
-  if (secureDnsBox->isChecked() &&\n      dnsCombo->itemData(pIdx).toString() == "OS Default")
+  if (secureDnsBox->isChecked() &&
+      dnsCombo->itemData(pIdx).toString() == "OS Default")
     pIdx = dnsCombo->findData("Cloudflare");
   dnsCombo->setCurrentIndex(pIdx);
 
